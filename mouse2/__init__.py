f717070f546721014207467dc844bd9e936f8e92
Mikhail Glagolev <mikhail.glagolev@gmail.com>
2023-02-22 20:53:51 +0300

    Add indices for rods. Update distribution version

--- mouse2/__init__.py
+++ mouse2/__init__.py
@@ -5,6 +5,6 @@ A toolkit for processing molecular dynamics simulation data
 with a focus on chiral ordering.
 """
 
-__version__ = "0.2.1"
+__version__ = "0.2.4"
 __author__ = 'Mikhail Glagolev, Anna Glagoleva'
 __credits__ = 'A. N. Nesmeyanov Institute of Organoelement Compounds RAS'
