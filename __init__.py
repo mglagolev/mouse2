2665288adf00cc8b7edae5bc01a51670f210d8c6
Mikhail Glagolev <mikhail.glagolev@gmail.com>
2025-05-05 10:15:15 +0300

    Changing the directory structure to align local and PyPI installations

--- __init__.py
+++ __init__.py
@@ -1,10 +1,10 @@
 """
-mouse2.tests
+mouse2
 
 A toolkit for processing molecular dynamics simulation data
 with a focus on chiral ordering.
 """
 
-__version__ = "0.2.22"
+__version__ = "0.2.31"
 __author__ = 'Mikhail Glagolev, Anna Glagoleva'
 __credits__ = 'A. N. Nesmeyanov Institute of Organoelement Compounds RAS'
